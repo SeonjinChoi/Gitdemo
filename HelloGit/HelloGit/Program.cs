9e6fea14e5a356e003009442f26398a9843f1746
SeonjinChoi <41276632+SeonjinChoi@users.noreply.github.com>
2018-07-17 12:19:11 +0900

    실습

---
+++ HelloGit/HelloGit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//How to debugging
+namespace HelloGit
+{
+    class Porgram
+    {
+        static void Main(string[] args)
+        {
+            int d = 0;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                d = d + 1; //F9로 중단점 설정 -> 오른쪽 버튼 눌러 조건 설정 가능
+
+                if (i == 5001)
+                    d = d / 0;
+            }
+
+            Console.WriteLine(d);
+        }
+    }
+ }      
